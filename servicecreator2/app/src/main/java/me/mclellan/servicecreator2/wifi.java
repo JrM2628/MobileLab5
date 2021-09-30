5f6078da4d8fbb0bc0aa6b59cc6b3bdd8d498d1f
jrm7162@rit.edu <jrm7162@rit.edu>
2021-09-30 13:56:47 -0400

    Added message to request contact information

--- servicecreator2/app/src/main/java/me/mclellan/servicecreator2/wifi.java
+++ servicecreator2/app/src/main/java/me/mclellan/servicecreator2/wifi.java
@@ -1,40 +1,79 @@
 package me.mclellan.servicecreator2;
 
+import android.annotation.SuppressLint;
 import android.app.Service;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
-import android.os.Binder;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
 import android.os.RemoteException;
-import android.widget.Toast;
+import android.provider.ContactsContract;
+import android.util.Log;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class wifi extends Service {
     private static WifiManager wm;
     private static WifiInfo wi;
+    private static ContentResolver contentResolver;
     static final int MSG_GET_SSID = 1;
+    static final int MSG_GET_CONTACTS = 2;
 
     //Handler stuff to handle incoming messages
     class IncomingHandler extends Handler {
         private Context applicationContext;
 
+        @SuppressLint("Range")
         @Override
         public void handleMessage(Message msg){
+            Bundle bundle;
+            Message m;
             switch (msg.what) {
+                // Reply to message with SSID content
                 case MSG_GET_SSID:
                     try {
-                        Message m = Message.obtain(null, MSG_GET_SSID, 0, 0);
-                        Bundle bundle = new Bundle();
+                        m = Message.obtain(null, MSG_GET_SSID, 0, 0);
+                        bundle = new Bundle();
                         bundle.putString("ssid", wi.getSSID());
                         m.setData(bundle);
                         msg.replyTo.send(m);
                     } catch (RemoteException e) {
                         e.printStackTrace();
+                    }
+                    break;
+
+                // Reply with contact details
+                case MSG_GET_CONTACTS:
+                    contentResolver = getContentResolver();
+                    Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, null);
+
+                    if (cursor.moveToFirst()){
+                        do {
+                            try {
+                                m = Message.obtain(null, MSG_GET_CONTACTS, 0, 0);
+                                bundle = new Bundle();
+
+                                String contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                                if(cursor.getInt(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.HAS_PHONE_NUMBER)) == 1) {
+                                    String phoneNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                                    bundle.putString("contact", contactName + ": " + phoneNumber);
+                                }
+                                m.setData(bundle);
+                                msg.replyTo.send(m);
+                            } catch (RemoteException e) {
+                                e.printStackTrace();
+                            } catch(Exception e){
+                                e.printStackTrace();
+                            }
+                        }while (cursor.moveToNext());
                     }
                     break;
                 default:
@@ -52,8 +91,8 @@ public class wifi extends Service {
     @Override
     // Creates WiFiManager and WiFi connection info objects
     public int onStartCommand(Intent intent, int flags, int startId){
-        this.wm = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        this.wi  = this.wm.getConnectionInfo();
+        wm = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        wi  = wm.getConnectionInfo();
         return Service.START_NOT_STICKY;
     }
 
--- serviceuser2/app/src/main/java/me/mclellan/serviceuser2/MainActivity.java
+++ serviceuser2/app/src/main/java/me/mclellan/serviceuser2/MainActivity.java
@@ -14,18 +14,26 @@ import android.os.RemoteException;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    static final int MSG_GET_SSID = 1;
+    static final int MSG_GET_CONTACTS = 2;
     Messenger mService = null;
 
     // Handles incoming messages (responses from service)
     class IncomingHandler extends Handler {
         @Override
         public void handleMessage(Message msg){
+            Bundle data;
+
             switch(msg.what){
-                case 1:
-                    Bundle data = msg.getData();
+                case MSG_GET_SSID:
+                    data = msg.getData();
                     String ssid = data.getString("ssid");
-                    Toast.makeText(getApplicationContext(), "FROM SERVICE INVOKER:" + ssid, Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "FROM SERVICE INVOKER: " + ssid, Toast.LENGTH_LONG).show();
+                    break;
+                case MSG_GET_CONTACTS:
+                    data = msg.getData();
+                    String contact = data.getString("contact");
+                    Toast.makeText(getApplicationContext(), "FROM SERVICE INVOKER: " + contact, Toast.LENGTH_LONG).show();
                     break;
                 default:
                     super.handleMessage(msg);
@@ -43,7 +51,15 @@ public class MainActivity extends AppCompatActivity {
 
             // Sends message of type '1' (SSID) to service
             try{
-                Message msg = Message.obtain(null, 1);
+                Message msg = Message.obtain(null, MSG_GET_SSID);
+                msg.replyTo = mMessenger;
+                mService.send(msg);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+            // Sends message of type '2' (Contact) to service
+            try{
+                Message msg = Message.obtain(null, MSG_GET_CONTACTS);
                 msg.replyTo = mMessenger;
                 mService.send(msg);
             } catch (RemoteException e) {
