0efa401fe3f104fdf8f4284b906f0d9580d639a7
jrm7162@rit.edu <jrm7162@rit.edu>
2021-09-30 01:21:09 -0400

    Modified service to send data back to invoker

--- servicecreator2/app/src/main/java/me/mclellan/servicecreator2/wifi.java
+++ servicecreator2/app/src/main/java/me/mclellan/servicecreator2/wifi.java
@@ -5,25 +5,67 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.Binder;
+import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.widget.Toast;
 
 public class wifi extends Service {
+    private static WifiManager wm;
+    private static WifiInfo wi;
+    static final int MSG_GET_SSID = 1;
+
+
+    class IncomingHandler extends Handler {
+        private Context applicationContext;
+
+        @Override
+        public void handleMessage(Message msg){
+            switch (msg.what) {
+                case MSG_GET_SSID:
+                    try {
+                        Message m = Message.obtain(null, MSG_GET_SSID, 0, 0);
+                        Bundle bundle = new Bundle();
+                        bundle.putString("ssid", wi.getSSID());
+                        m.setData(bundle);
+                        msg.replyTo.send(m);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+    final Messenger mMessenger = new Messenger(new IncomingHandler());
+
+
+
     public wifi() {
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId){
-        WifiManager wm = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        WifiInfo wi = wm.getConnectionInfo();
-        String ssid = wi.getSSID();
-        Toast.makeText(this, ssid, Toast.LENGTH_LONG).show();
+        this.wm = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        this.wi  = this.wm.getConnectionInfo();
+
+        //String ssid = this.wi.getSSID();
+        //Toast.makeText(this, ssid, Toast.LENGTH_LONG).show();
+
         return Service.START_NOT_STICKY;
+    }
+
+    public void onCreate(){
+        return;
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mMessenger.getBinder();
     }
 }
--- serviceuser2/app/src/main/java/me/mclellan/serviceuser2/MainActivity.java
+++ serviceuser2/app/src/main/java/me/mclellan/serviceuser2/MainActivity.java
@@ -5,15 +5,65 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.ComponentName;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.provider.ContactsContract;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Messenger mService = null;
+    boolean mIsBound;
+
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg){
+            switch(msg.what){
+                case 1:
+                    Bundle data = msg.getData();
+                    String ssid = data.getString("ssid");
+                    Toast.makeText(getApplicationContext(), "FROM SERVICE INVOKER:" + ssid, Toast.LENGTH_LONG).show();
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+    final Messenger mMessenger = new Messenger(new IncomingHandler());
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            mService = new Messenger(service);
+            Toast.makeText(getApplicationContext(), "Attached", Toast.LENGTH_LONG).show();
+
+            try{
+                Message msg = Message.obtain(null, 1);
+                msg.replyTo = mMessenger;
+                mService.send(msg);
+
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mService = null;
+            Toast.makeText(getApplicationContext(), "Disconnected", Toast.LENGTH_LONG).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +74,6 @@ public class MainActivity extends AppCompatActivity {
         i.putExtra("message", "Hello World AAAA");
         i.setComponent(new ComponentName("me.mclellan.servicecreator2", "me.mclellan.servicecreator2.wifi"));
         startService(i);
+        bindService(i, mConnection, Context.BIND_AUTO_CREATE);
     }
 }
